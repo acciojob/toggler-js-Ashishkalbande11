740b239e95f3ba6e53e958435a21d241dfa12731
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-09-23 08:36:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const checkBox = document.querySelectorAll(".toggle");
+
+checkBox.forEach((check) =>{
+	check.addEventListener('change', () => {
+		const checkedBoxes = [...checkBox].filter((cd)=>cd.checked);
+
+		if(checkedBoxes.length > 2){
+			checkedBoxes[1].checked = false;
+		}
+	});
+});
